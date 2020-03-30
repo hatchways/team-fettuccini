1c72083888fc7c09cf6169b3b50d61b46acc7b1c
Pavel Machuca <pavelmachuca@hotmail.com>
2020-03-30 19:31:42 -0400

    * polished styling * added labels

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Typography } from "@material-ui/core";
-import { Route, Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 export default class Login extends Component {
   constructor(props) {
@@ -20,41 +20,36 @@ export default class Login extends Component {
   }
   async handleSubmit(event) {
     event.preventDefault()
-    await this.props.login(
-      {
-        email: this.state.email,
-        password: this.state.password
-      }
-    )
-    const { id, token } = this.props
-    if (token) {
-      await this.props.getTransactions(id, token)
-      this.props.getPortfolio(id, token)
-    } else {
-      alert('Invalid credentials, please try again')
-    }
   }
 
 
   render() {
+    if (this.props.token) {
+      return <Redirect to='/' />
+    }
+
     return (
       <>
         <Typography className="Form-title">Sign In</Typography>
         <form onSubmit={this.handleSubmit}>
+          <label for="email">Email:</label>
           <input
             className='Form-text-input'
             name="email"
-            type="text"
+            type="email"
             value={this.state.email}
             onChange={this.handleChange}
-            placeholder="Enter Email" />
+            placeholder="johndoe@gmail.com"
+            required />
+          <label for="password">Password:</label>
           <input
             className='Form-text-input'
             name="password"
             type="password"
             value={this.state.password}
             onChange={this.handleChange}
-            placeholder="Enter Password" />
+            placeholder="Password"
+            required />
           <button className='Form-submit' type='submit'>Sign In</button>
         </form>
       </>
--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Typography } from "@material-ui/core";
-import { Route, Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 
 const authStyle = theme => ({
@@ -15,71 +15,95 @@ class SignUp extends Component {
     this.state = {
       name: '',
       email: '',
-      password: ''
+      password1: '',
+      password2: '',
+      error: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleConfirmPassword = this.handleConfirmPassword.bind(this)
   }
 
   handleChange(event) {
     this.setState({
+      ...this.state,
       [event.target.name]: event.target.value
     })
   }
 
-  async handleSubmit(event) {
+  handleSubmit(event) {
     event.preventDefault()
-    await this.props.login(
-      {
-        email: this.state.email,
-        password: this.state.password
-      }
-    )
-    const { id, token } = this.props
-    if (token) {
-      await this.props.getTransactions(id, token)
-      this.props.getPortfolio(id, token)
-    } else {
-      alert('Invalid credentials, please try again')
+
+    let error = ''
+    if (this.state.password1 !== this.state.password2) {
+      error = "Passwords must match"
+      this.setState({ ...this.state, error })
+      return
     }
+
+    window.alert('passed')
+    this.setState({ ...this.state, error })
+  }
+  handleConfirmPassword(event) {
+    let error = this.state.password1 !== event.target.value
+      ? "Passwords must match"
+      : '';
+
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+      error
+    })
   }
 
   render() {
     if (this.props.token) {
       return <Redirect to='/' />
     }
+
+    const errorMessage = this.state.error.length !== 0 ? <p className="Form-warning">{this.state.error}</p> : null;
+
     return (
       <>
         <Typography className="Form-title">Sign up</Typography>
         <form onSubmit={this.handleSubmit}>
+          <label for="name">Name:</label>
           <input
             className='Form-text-input'
             name="name"
             type="text"
             value={this.state.name}
             onChange={this.handleChange}
-            placeholder="Enter Name" />
+            placeholder="Enter your Name"
+            required />
+          <label for="email">Email:</label>
           <input
             className='Form-text-input'
             name="email"
             type="email"
             value={this.state.email}
             onChange={this.handleChange}
-            placeholder="Enter Email" />
+            placeholder="Enter your Email"
+            required />
+          <label for="email">Password:</label>
           <input
             className='Form-text-input'
             name="password1"
             type="password"
+            minLength='6'
             value={this.state.password1}
             onChange={this.handleChange}
-            placeholder="Enter Password" />
+            placeholder="Enter Password"
+            required />
+          <label for="email">Confirm Password:</label>
           <input
             className='Form-text-input'
             name="password2"
             type="password"
             value={this.state.password2}
-            onChange={this.handleChange}
+            onChange={this.handleConfirmPassword}
             placeholder="Enter Password Again" />
+          {errorMessage}
           <button className='Form-submit' type='submit'>Sign Up</button>
         </form>
       </>

7440130b4611f68ffaf46002ec69407af1a9622a
Pavel Machuca <pavelmachuca@hotmail.com>
2020-03-30 11:08:12 -0400

    * added header comp * renamed to landing -> signup

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import SignUp from "./pages/SignUp";
 
 import "./App.css";
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Route path="/" component={SignUp} />
       </BrowserRouter>
     </MuiThemeProvider>
   );

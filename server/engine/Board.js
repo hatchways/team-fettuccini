9aa764696595bccdda88ed8902277b80a8e151eb
Bhargava Guntoori <bguntoori@leanindustries.com>
2020-03-31 14:20:33 -0400

    Created Dictionary. Removed duplicates and plurals where singular is present.

--- server/engine/Board.js
+++ server/engine/Board.js
@@ -1,5 +1,6 @@
 const GameWord = require("./GameWord.js");
 const word_state = require("./WordStates.js");
+const Dictionary = require("./Dictionary");
 
 class Board {
 
@@ -7,9 +8,41 @@ class Board {
 		//TODO create list of 25 words along with the word state.
 		this.word_list = new Array(25);
 		var i = 0;
+		var dictSize = Dictionary.length;
+		var wordMap = {};
+		var red = 9;
+		var blue = 8;
+		var civilian = 7;
+		var assassin = 1;
 		for (i = 0;i<25;i++)
 		{
-			this.word_list[i]=new GameWord("word"+i, word_state.BLUE);
+			var word;
+			do
+			{
+				var randInt = Math.floor(Math.random() * dictSize);
+				word = Dictionary[randInt];
+			} while (word in wordMap);
+			wordMap[word] = true;
+			var randInt = Math.floor(Math.random()*(25-i));
+			this.state = word_state.RED;
+			if (randInt>=red) {
+				if (randInt>=red+blue) {
+					if (randInt>=red+blue+civilian) {
+						this.state = word_state.ASSASSIN;
+						assassin--;
+					} else {
+						this.state = word_state.CIVILIAN;
+						civilian--;
+					}
+				} else {
+					this.state = word_state.BLUE;
+					blue--;
+				}
+			} else {
+				red--;
+			}
+			this.word_list[i]=new GameWord(word, this.state);
+			console.log("Created new word for board "+word+" "+this.state);
 		}
 	}
 

5edb6ebfcda45378a02286413f2eb71bb2a9c52d
Pavel Machuca <pavelmachuca@hotmail.com>
2020-04-02 12:37:29 -0400

    Update client/src/ProtectedRoute.js
    made conditional return of Route or Redirect
    
    Co-Authored-By: Aecio <aecio.rcv@gmail.com>

--- client/src/ProtectedRoute.js
+++ client/src/ProtectedRoute.js
@@ -4,21 +4,13 @@ import { Redirect, Route } from "react-router-dom";
 import auth from './components/auth/auth'
 
 export default function PrivateRoute({ children, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        auth.isAuthenticated() ? (
-          children
-        ) : (
-            <Redirect
-              to={{
-                pathname: "/",
-                state: { from: location }
-              }}
-            />
-          )
-      }
+  return auth.isAuthenticated() ? (
+    <Route {...rest}>{children}</Route>
+  ) : (
+    <Redirect
+      to={{
+        pathname: "/"
+      }}
     />
   );
 }

bb5a2f77da07f84d071012ea7d930d411a01136a
Bhargava Guntoori <bguntoori@leanindustries.com>
2020-04-01 20:07:57 -0400

    Implemented all test cases for Game.js except for the field agent move function.

--- server/test/Game.spec.js
+++ server/test/Game.spec.js
@@ -5,6 +5,7 @@ const isNotNull = assert.isNotNull;
 const isNotNaN = assert.isNotNaN;
 const isDefined = assert.isDefined;
 const equal = assert.equal;
+const notEqual = assert.notEqual;
 const {Game, gameState} = require("../engine/Game.js");
 const GameWord = require("../engine/GameWord.js");
 const WordStates = require("../engine/WordStates.js");
@@ -52,11 +53,240 @@ describe('Game', () => {
 		return infoObj;
 	}
 
+	//Check that board is initialized
   it("it should have a non-empty board", () => {
 	let g = new Game();
 	isNotNull(g, 'The board must be initialized when the Game is initialized');
 	isNotNaN(g, 'The board must be initialized when the Game is initialized');
 	isDefined(g, 'Board must be defined');
+  });
+
+  //nextTurn test
+  it ("Turn cycle should be RED_SPY->RED_FIELD->BLUE_SPY->BLUE_FIELD", () => {
+	  var infoObj = metaGame();
+	  var g = infoObj.g;
+	  equal(g.getState(), gameState.RED_SPY);
+	  g.nextTurn();
+	  equal(g.getState(), gameState.RED_FIELD);
+	  g.nextTurn();
+	  equal(g.getState(), gameState.BLUE_SPY);
+	  g.nextTurn();
+	  equal(g.getState(), gameState.BLUE_FIELD);
+	  g.nextTurn();
+	  equal(g.getState(), gameState.RED_SPY);
+
+  });
+
+  //Spy hint tests
+  it ("Spy hint not being processed properly for red spymaster", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.nextSpyHint(1, "hi");
+	equal(g.getState(), gameState.RED_FIELD);
+	equal(g.numGuessesLeft,1);
+	equal(g.spyHint, "hi");
+  });
+
+  it ("Spy hint not being processed properly for blue spymaster", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.BLUE_SPY;
+	g.nextSpyHint(1, "hi");
+	equal(g.getState(), gameState.BLUE_FIELD);
+	equal(g.numGuessesLeft,1);
+	equal(g.spyHint, "hi");
+  });
+
+  //Check if won tests
+  it ("Red should win", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.redLeft = 0;
+	g.checkIfWon();
+	equal(g.getState(), gameState.RED_WON);
+  });
+
+  it ("Blue should win", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.blueLeft = 0;
+	g.checkIfWon();
+	equal(g.getState(), gameState.BLUE_WON);
+  });
+
+  it ("Nobody should win yet", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.redLeft = 9;
+	g.blueLeft = 8;
+	g.checkIfWon();
+	notEqual(g.getState(), gameState.BLUE_WON);
+	notEqual(g.getState(), gameState.RED_WON);
+  });
+
+  it ("Failing to detect that the game is over when blue wins", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.BLUE_WON;
+	equal(g.isGameOver(), true);
+  });
+
+  //Checking if game is over
+  it ("Failing to detect that the game is over when red wins", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.RED_WON;
+	equal(g.isGameOver(), true);
+  });
+
+  it ("Falsely detecting a winner", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.RED_SPY;
+	equal(g.isGameOver(), false);
+	g.state = gameState.RED_FIELD;
+	equal(g.isGameOver(), false);
+	g.state = gameState.BLUE_SPY;
+	equal(g.isGameOver(), false);
+	g.state = gameState.BLUE_FIELD;
+	equal(g.isGameOver(), false);
+  });
+
+  //Voluntarily ending turn of field agent
+  it ("Should end turn of RED Field agent", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.RED_FIELD;
+	g.madeGuess = true;
+	g.endTurn();
+	equal(g.state, gameState.BLUE_SPY);
+	equal(g.madeGuess, false);
+  });
+
+  it ("Should end turn of BLUE field agent", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.BLUE_FIELD;
+	g.madeGuess = true;
+	g.endTurn();
+	equal(g.state, gameState.RED_SPY);
+	equal(g.madeGuess, false);
+  });
+
+  it ("Should not end turn when 0 guesses have been made", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.BLUE_FIELD;
+	g.madeGuess = false;
+	g.endTurn();
+	equal(g.state, gameState.BLUE_FIELD);
+	equal(g.madeGuess, false);
+  });
+
+  it ("Should not end turn when during SPYMASTER turns", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.BLUE_SPY;
+	g.madeGuess = true;
+	g.endTurn();
+	equal(g.state, gameState.BLUE_SPY);
+	equal(g.madeGuess, true);
+
+	g.state = gameState.RED_SPY;
+	g.madeGuess = true;
+	g.endTurn();
+	equal(g.state, gameState.RED_SPY);
+	equal(g.madeGuess, true);
+
+	g.state = gameState.BLUE_SPY;
+	g.madeGuess = false;
+	g.endTurn();
+	equal(g.state, gameState.BLUE_SPY);
+	equal(g.madeGuess, false);
+
+	g.state = gameState.RED_SPY;
+	g.madeGuess = false;
+	g.endTurn();
+	equal(g.state, gameState.RED_SPY);
+	equal(g.madeGuess, false);
+  });
+
+  //Board info.
+  it ("Board info needs to return info of all cards on the board", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	var words = g.board.get_words();
+
+	words[infoObj.ri[0]].choose();
+	words[infoObj.ri[1]].choose();
+	words[infoObj.ri[2]].choose();
+
+	words[infoObj.bi[0]].choose();
+	words[infoObj.bi[1]].choose();
+	words[infoObj.bi[2]].choose();
+
+	words[infoObj.ci[0]].choose();
+	words[infoObj.ci[1]].choose();
+	words[infoObj.ci[2]].choose();
+
+	words[infoObj.assi].choose();
+
+	var boardValues = g.getBoardInfo();
+
+	var redI = 0;
+	var blueI = 0;
+	var civI = 0;
+	
+	//console.log(g.getBoardInfo());
+	for (var i = 0;i<boardValues.length;i++)
+	{
+		//console.log(boardValues[i]+" "+i+" "+infoObj.ri[redI]+" "+infoObj.bi[blueI]+" "+infoObj.ci[civI]+" "+infoObj.assi);
+		if (redI<3 && i==infoObj.ri[redI]) {
+			equal(boardValues[infoObj.ri[redI]], "_RED");
+			redI++;
+		} else if (blueI<3 && i==infoObj.bi[blueI]) {
+			equal(boardValues[infoObj.bi[blueI]], "_BLUE");
+			blueI++;
+		} else if (civI<3 &&i==infoObj.ci[civI]) {
+			equal(boardValues[infoObj.ci[civI]], "_CIVILIAN");
+			civI++;
+		} else if (i==infoObj.assi) {
+			equal(boardValues[infoObj.assi], "_ASSASSIN");
+		} else {
+			equal(boardValues[i],words[i].get_val());
+		}
+	}
+  });
+
+  //Reset
+  it ("Reset should set the board to its initial state", () => {
+	var infoObj = metaGame();
+	var g = infoObj.g;
+	g.state = gameState.BLUE_FIELD;
+	g.madeGuess = true;
+	g.redLeft = 0;
+	g.blueLeft = 0;
+	g.numGuessesLeft = 1;
+	g.spyHint = "hi";
+	var temp = g.getBoardInfo();
+	g.reset();
+
+	equal(g.state, gameState.RED_SPY);
+	equal(g.madeGuess, false);
+	equal(g.redLeft, 9);
+	equal(g.blueLeft, 8);
+	equal(g.numGuessesLeft, 0);
+	equal(g.spyHint, "");
+	var temp2 = g.getBoardInfo();
+
+	var eq = true;
+	for (var i = 0;i<temp.length;i++) {
+		if (temp[i] != temp2[i]) {
+			eq = false;
+			break;
+		}
+	}
+	equal(eq, false);
   });
 
   it ("Turn cycle should be RED_SPY->RED_FIELD->BLUE_SPY->BLUE_FIELD", () => {

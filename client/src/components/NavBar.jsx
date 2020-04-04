2f92c09f06a65e172b1f5c520340b5fe943bb7ac
Pavel Machuca <pavelmachuca@hotmail.com>
2020-04-03 22:53:19 -0400

    aligned navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,16 +4,43 @@ import { Typography, Button, Box } from "@material-ui/core";
 
 import auth from './auth/auth'
 
-export default function () {
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  header: {
+    display: "flex",
+    flexWrap: 'wrap-reverse',
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    textAlign: "center"
+  },
+  invisible: {
+    opacity: "0",
+    "&:hover": {
+      cursor: "default"
+    }
+  },
+  navSides: {
+    flexGrow: '1'
+  },
+  navMain: {
+    flexGrow: "12"
+  }
+});
+
+export default function (props) {
   let history = useHistory();
+  const classes = useStyles(props);
 
   return (
-    <Box display="flex" className="header">
-      <Typography variant="h2">
+    <Box className={classes.header}>
+      <Button className={`${classes.invisible} ${classes.navSides}`}>&nbsp;</Button>
+      <Typography variant="h2" className={classes.navMain}>
         CODINGWORDS
       </Typography>
       {auth.isAuthenticated() ? <Button
-        alignSelf="flex-end"
+        className={classes.navSides}
+
         variant="contained"
         color="primary"
         onClick={() => {

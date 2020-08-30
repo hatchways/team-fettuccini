b2cbe280f4e1e3711434a9289909d5bec2b42d3a
Bhargava Guntoori <bguntoori@leanindustries.com>
2020-08-30 15:32:08 -0400

    Refactored the statistics.js file for better multi-user performance.

--- server/routes/statistics.js
+++ server/routes/statistics.js
@@ -6,164 +6,24 @@ const NodeCache = require('node-cache');
 const router = express.Router();
 const requestCache = new NodeCache({stdTTL: 30, checkperiod: 5 });
 const users = new Map();
+const sortBy = new Map();
 
 async function requestAndCache() {
 	
 	console.log("Fetching new data");
 	await User.find({}, function (err, allUsers) {
-		console.log(allUsers[0]);
-		let wins = [];
-		let losses = [];
-		let opponentsHits = [];
-		let correctHits = [];
-		let civiliansHits = [];
-		let assassinsHits = [];
-		let opponentsAssists = [];
-		let correctAssists = [];
-		let civiliansAssists = [];
-		let assassinsAssists = [];
-		let numHints = [];
-		let correctGuessPercent = [];
-		let correctAssistsPercent = [];
-		let correctGuessesPerHint = [];
-		let resultsCached = 0;
-		
-		console.log("Started creating promises");
+		requestCache.set("allUsers", allUsers);
 
-		new Promise(() => {
-			console.log("Creating user map");
-			users.clear();
-			allUsers.forEach((element) => {
-				users.set(String(element._id), element);
-			});
-			console.log("Created user map");
-			resultsCached++;
-			console.log(resultsCached);
+		console.log("Creating user map");
+		sortBy.clear();
+		users.clear();
+		allUsers.forEach((element) => {
+			users.set(String(element._id), element);
 		});
-		
-		wins = [...allUsers];
-		wins.sort(function(x, y) { 
-			console.log("hello ", x);
-			console.log("numWins ", x.numWins);
-			const val1 = x.numWins;
-			const val2 = y.numWins;
-			return val2 - val1;
-		});
-
-		losses = [...allUsers];
-		losses.sort(function(x, y) {
-			const val1 = x.numLosses;
-			const val2 = y.numLosses;
-			return val1 - val2;
-		});
-
-		opponentsHits = [...allUsers];
-		opponentsHits.sort(function(x, y) {
-			const val1 = x.opponentsHits;
-			const val2 = y.opponentsHits;
-			return val1 - val2;
-		});
-
-		
-		correctHits = [...allUsers];
-		correctHits.sort(function(x, y) {
-			const val1 = x.correctHits;
-			const val2 = y.correctHits;
-			return val1 - val2;
-		});
-
-		
-		civiliansHits = [...allUsers];
-		civiliansHits.sort(function(x, y) {
-			const val1 = x.civiliansHits;
-			const val2 = y.civiliansHits;
-			return val1 - val2;
-		});
-
-		
-		assassinsHits = [...allUsers];
-		assassinsHits.sort(function(x, y) {
-			const val1 = x.assassinsHits;
-			const val2 = y.assassinsHits;
-			return val1 - val2;
-		});
-
-		
-		opponentsAssists = [...allUsers];
-		opponentsAssists.sort(function(x, y) {
-			const val1 = x.opponentsAssists;
-			const val2 = y.opponentsAssists;
-			return val1 - val2;
-		});
-
-		
-		correctAssists = [...allUsers];
-		correctAssists.sort(function(x, y) {
-			const val1 = x.correctAssists;
-			const val2 = y.correctAssists;
-			return val1 - val2;
-		});
-		
-		civilianAssists = [...allUsers];
-		civiliansAssists.sort(function(x, y) {
-			const val1 = x.civiliansAssists;
-			const val2 = y.civiliansAssists;
-			return val1 - val2;
-		});
-
-		
-		assassinsAssists = [...allUsers];
-		assassinsAssists.sort(function(x, y) {
-			const val1 = x.assassinsAssists;
-			const val2 = y.assassinsAssists;
-			return val1 - val2;
-		});
-		
-		numHints = [...allUsers];
-		numHints.sort(function(x, y) {
-			const val1 = x.numHints;
-			const val2 = y.numHints;
-			return val1 - val2;
-		});
-		
-		correctGuessPercent = [...allUsers];
-		correctGuessPercent.sort(function(x, y) {
-			const val1 = Number.parseFloat(x.correctHits/(x.correctHits+x.assassinsHits+x.civiliansHits+x.opponentsHits)).toFixed(2);
-			const val2 = Number.parseFloat(y.correctHits/(y.correctHits+y.assassinsHits+y.civiliansHits+y.opponentsHits)).toFixed(2);
-			return val1 - val2;
-		});
-		
-		correctAssistsPercent = [...allUsers]
-		correctAssistsPercent.sort(function(x, y) {
-			const val1 = Number.parseFloat(x.correctAssists/(x.correctAssists+x.assassinsAssists+x.civiliansAssists+x.opponentsAssists)).toFixed(2);
-			const val2 = Number.parseFloat(y.correctAssists/(y.correctAssists+y.assassinsAssists+y.civiliansAssists+y.opponentsAssists)).toFixed(2);
-			return val1 - val2;
-		});
-		
-		correctGuessesPerHint = [...allUsers];
-		correctGuessesPerHint.sort(function(x, y) {
-			const val1 = Number.parseFloat(x.correctAssists/(x.numHints)).toFixed(2);
-			const val2 = Number.parseFloat(y.correctAssists/(y.numHints)).toFixed(2);
-			return val1 - val2;
-		});
-		
-		requestCache.set("numWins", wins);
-		requestCache.set("numLosses", losses);
-		requestCache.set("opponentsHits", opponentsHits);
-		requestCache.set("correctHits", correctHits);
-		requestCache.set("civiliansHits", civiliansHits);
-		requestCache.set("assassinsHits", assassinsHits);
-		requestCache.set("opponentsAssists", opponentsAssists);
-		requestCache.set("correctAssists", correctAssists);
-		requestCache.set("civiliansAssists", civiliansAssists);
-		requestCache.set("assassinsAssists", assassinsAssists);
-		requestCache.set("numHints", numHints);
-		requestCache.set("correctGuessPercent", correctGuessPercent);
-		requestCache.set("correctAssistsPercent", correctAssistsPercent);
-		requestCache.set("correctGuessesPerHint", correctGuessesPerHint);
+		console.log("Created user map");
 	}).catch((err)=>{console.log("Error getting users from database", err)});
 	
-	
+	console.log("Processing db data");
 }
 
 router.get("/byuser", async (req, res) => {
@@ -197,8 +57,128 @@ router.get("/standings", async (req, res) => {
 		}
 		
 		let pageSkip = req.query.page-1;
-		let sortBy = req.query.sortBy;
-		return res.status(200).send(requestCache.get(sortBy));
+		let sorting = req.query.sortBy;
+		console.log("fetching standings");
+		const cacheFunction = (sortFunc, sortByVal) => {
+			console.log("In caching function")
+			if (!sortBy.has(sortByVal)) {
+				const allUsers = requestCache.get("allUsers");
+				const sortedUsers = [...allUsers];
+				sortedUsers.sort(sortFunc);
+				sortBy.set(sortByVal, sortedUsers);
+			}
+			console.log("Returning value");
+			return res.status(200).send(sortBy.get(sortByVal));
+		}
+		
+		if (sorting=="numWins") {
+			const sortFunction = function(x, y) {
+					const val1 = x.numWins;
+					const val2 = y.numWins;
+					return val2 - val1;
+				};
+			return cacheFunction(sortFunction, "numWins");
+		} else if (sorting=="numLosses") {
+			const sortFunction = function(x, y) {
+					const val1 = x.numLosses;
+					const val2 = y.numLosses;
+					return val2 - val1;
+				};
+			return cacheFunction(sortFunction, "numLosses");
+		} else if (sorting == "opponentsHits") {
+			const sortFunction = function(x, y) {
+					const val1 = x.opponentsHits;
+					const val2 = y.opponentsHits;
+					return val2 - val1;
+				};
+			return cacheFunction(sortFunction, "opponentsHits");
+		} else if (sorting == "correctHits") {
+			const sortFunction = function(x, y) {
+					const val1 = x.correctHits;
+					const val2 = y.correctHits;
+					return val2 - val1;
+				};
+			return cacheFunction(sortFunction, "correctHits");
+		} else if (sorting == "assassinsHits") {
+			const sortFunction = function(x, y) {
+					const val1 = x.assassinsHits;
+					const val2 = y.assassinsHits;
+					return val2 - val1;
+				};
+			return cacheFunction(sortFunction, "assassinsHits");
+		} else if (sorting == "civiliansHits") {
+			const sortFunction = function(x, y) {
+					const val1 = x.civiliansHits;
+					const val2 = y.civiliansHits;
+					return val2 - val1;
+				};
+			return cacheFunction(sortFunction, "civiliansHits");
+		} else if (sorting == "opponentsAssists") {
+			const sortFunction = function(x, y) {
+					const val1 = x.opponentsAssists;
+					const val2 = y.opponentsAssists;
+					return val2 - val1;
+				};
+			return cacheFunction(sortFunction, "opponentsAssists");
+		} else if (sorting == "civiliansAssists") {
+			const sortFunction = function(x, y) {
+					const val1 = x.civiliansAssists;
+					const val2 = y.civiliansAssists;
+					return val2 - val1;
+				}
+			return cacheFunction(sortFunction, "civiliansAssists");
+		} else if (sorting == "correctAssists") {
+			const sortFunction =function(x, y) {
+					const val1 = x.correctAssists;
+					const val2 = y.correctAssists;
+					return val2 - val1;
+				}
+			return cacheFunction(sortFunction, "correctAssists");
+		} else if (sorting == "assassinsAssists") {
+			const sortFunction = function(x, y) {
+					const val1 = x.assassinsAssists;
+					const val2 = y.assassinsAssists;
+					return val2 - val1;
+				}
+			return cacheFunction(sortFunction, "assassinsAssists");
+		} else if (sorting == "numHints") {
+			const sortFunction = function(x, y) {
+					const val1 = x.numHints;
+					const val2 = y.numHints;
+					return val2 - val1;
+				}
+			return cacheFunction(sortFunction, "numHints");
+		} else if (sorting == "correctGuessPercent") {
+			const sortFunction = function(x, y) {
+					const val1 = Number.parseFloat(x.correctHits/(x.correctHits+x.assassinsHits+x.civiliansHits+x.opponentsHits)).toFixed(2);
+					const val2 = Number.parseFloat(y.correctHits/(y.correctHits+y.assassinsHits+y.civiliansHits+y.opponentsHits)).toFixed(2);
+					return val2 - val1;
+				}
+			return cacheFunction(sortFunction, "correctGuessPercent");
+		} else if (sorting == "correctAssistsPercent") {
+			const sortFunction = function(x, y) {
+					const val1 = Number.parseFloat(x.correctAssists/(x.correctAssists+x.assassinsAssists+x.civiliansAssists+x.opponentsAssists)).toFixed(2);
+					const val2 = Number.parseFloat(y.correctAssists/(y.correctAssists+y.assassinsAssists+y.civiliansAssists+y.opponentsAssists)).toFixed(2);
+					return val2 - val1;
+				}
+			return cacheFunction(sortFunction, "correctAssistsPercent");
+		} else if (sorting == "correctGuessesPerHint") {
+			const sortFunction = function(x, y) {
+				const val1 = Number.parseFloat(x.correctAssists/(x.numHints)).toFixed(2);
+				const val2 = Number.parseFloat(y.correctAssists/(y.numHints)).toFixed(2);
+				return val2 - val1;
+			}
+			return cacheFunction(sortFunction, "correctGuessesPerHint");
+		} else if (sorting == "winPercent") {
+			const sortFunction = function(x, y) {
+				const val1 = Number.parseFloat((x.numWins)/(x.numWins+x.numLosses)).toFixed(2);
+				const val2 = Number.parseFloat((y.numWins)/(y.numWins+y.numLosses)).toFixed(2);
+				return val2 - val1;
+			}
+			return cacheFunction(sortFunction, "winPercent");
+		}
+		
+		return res.status(200).send();
 		
 	} catch (error) {
 		
